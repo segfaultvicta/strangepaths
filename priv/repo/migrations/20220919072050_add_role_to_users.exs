3af6a20f8467a91d6edf2a31397e16ab15005f0e
JC Cantwell <jon.c.cantwell@gmail.com>
2025-10-29 18:36:41 -0400

    change user role definition in migration

--- priv/repo/migrations/20220919072050_add_role_to_users.exs
+++ priv/repo/migrations/20220919072050_add_role_to_users.exs
@@ -2,7 +2,7 @@ defmodule Strangepaths.Repo.Migrations.AddRoleToUsers do
   use Ecto.Migration
 
   def change do
-    create_query = "CREATE TYPE user_role AS ENUM ('user', 'admin', 'god')"
+    create_query = "CREATE TYPE user_role AS ENUM ('user', 'dragon')"
     drop_query = "DROP TYPE user_role"
     execute(create_query, drop_query)
 
