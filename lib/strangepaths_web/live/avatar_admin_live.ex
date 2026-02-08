c8d7f5c1229bde7804bdfadcc37ef76537c51cd7
JC Cantwell <jon.c.cantwell@gmail.com>
2026-02-08 16:00:43 -0500

    add Awoken art, change the way avatar creation works to rely on image upload

--- lib/strangepaths_web/live/avatar_admin_live.ex
+++ lib/strangepaths_web/live/avatar_admin_live.ex
@@ -10,9 +10,10 @@ defmodule StrangepathsWeb.AvatarAdminLive do
     if socket.assigns.current_user.role == :dragon do
       {:ok,
        socket
-       |> assign(:avatars, Accounts.list_avatars())
+       |> assign_avatars()
        |> assign(:editing_avatar, nil)
-       |> assign(:creating_avatar, false)}
+       |> assign(:creating_avatar, false)
+       |> allow_upload(:avatar_image, accept: ~w(.png .jpg .jpeg .webp), max_entries: 1)}
     else
       {:ok,
        socket
@@ -30,26 +31,34 @@ defmodule StrangepathsWeb.AvatarAdminLive do
     {:noreply, assign(socket, :creating_avatar, false)}
   end
 
+  def handle_event("validate_upload", _params, socket) do
+    {:noreply, socket}
+  end
+
   def handle_event("create", params, socket) do
-    filepath = "/images/avatars/#{params["filename"]}"
+    filepath = handle_avatar_upload(socket)
 
-    attrs = %{
-      filepath: filepath,
-      category: params["category"],
-      display_name: params["display_name"],
-      public: params["public"] == "true"
-    }
+    if filepath do
+      attrs = %{
+        filepath: filepath,
+        category: params["category"],
+        display_name: params["display_name"],
+        public: params["public"] == "true"
+      }
 
-    case Accounts.create_avatar(attrs) do
-      {:ok, _avatar} ->
-        {:noreply,
-         socket
-         |> assign(:avatars, Accounts.list_avatars())
-         |> assign(:creating_avatar, false)
-         |> put_flash(:info, "Avatar created!")}
+      case Accounts.create_avatar(attrs) do
+        {:ok, _avatar} ->
+          {:noreply,
+           socket
+           |> assign_avatars()
+           |> assign(:creating_avatar, false)
+           |> put_flash(:info, "Avatar created!")}
 
-      {:error, _changeset} ->
-        {:noreply, put_flash(socket, :error, "Failed to create avatar")}
+        {:error, _changeset} ->
+          {:noreply, put_flash(socket, :error, "Failed to create avatar")}
+      end
+    else
+      {:noreply, put_flash(socket, :error, "Please select an image file")}
     end
   end
 
@@ -74,7 +83,7 @@ defmodule StrangepathsWeb.AvatarAdminLive do
       {:ok, _avatar} ->
         {:noreply,
          socket
-         |> assign(:avatars, Accounts.list_avatars())
+         |> assign_avatars()
          |> assign(:editing_avatar, nil)
          |> put_flash(:info, "Avatar updated!")}
 
@@ -90,7 +99,7 @@ defmodule StrangepathsWeb.AvatarAdminLive do
       {:ok, _avatar} ->
         {:noreply,
          socket
-         |> assign(:avatars, Accounts.list_avatars())
+         |> assign_avatars()
          |> put_flash(:info, "Avatar deleted!")}
 
       {:error, _changeset} ->
@@ -102,6 +111,44 @@ defmodule StrangepathsWeb.AvatarAdminLive do
     avatar = Accounts.get_avatar!(id)
     Accounts.update_avatar(avatar, %{public: !avatar.public})
 
-    {:noreply, assign(socket, :avatars, Accounts.list_avatars())}
+    {:noreply, assign_avatars(socket)}
   end
+
+  defp assign_avatars(socket) do
+    avatars = Accounts.list_avatars()
+
+    avatars_by_category =
+      avatars
+      |> Enum.group_by(fn a -> a.category || "general" end)
+      |> Enum.sort_by(fn {cat, _} -> cat end)
+
+    categories = Enum.map(avatars_by_category, fn {cat, _} -> cat end)
+
+    socket
+    |> assign(:avatars_by_category, avatars_by_category)
+    |> assign(:categories, categories)
+  end
+
+  defp handle_avatar_upload(socket) do
+    dest_dir =
+      Path.join([:code.priv_dir(:strangepaths), "static", "uploads", "avatars"])
+
+    File.mkdir_p!(dest_dir)
+
+    paths =
+      consume_uploaded_entries(socket, :avatar_image, fn %{path: path}, entry ->
+        filename =
+          "avatar_#{System.system_time(:second)}_#{entry.client_name}"
+
+        dest = Path.join(dest_dir, filename)
+        File.cp!(path, dest)
+        {:ok, Routes.static_path(socket, "/uploads/avatars/#{filename}")}
+      end)
+
+    List.first(paths)
+  end
+
+  def friendly_error(:too_large), do: "Image too large"
+  def friendly_error(:too_many_files), do: "Too many files"
+  def friendly_error(:not_accepted), do: "Unacceptable file type"
 end
